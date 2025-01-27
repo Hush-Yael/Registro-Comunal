a7f4740b4d392a7ab592cd203731cb950fe12b8b
Yael <srjean21@gmail.com>
2025-01-27 17:46:04 -0400

    (changes) Se cambió cómo funciona el hook y se agregó la opción de "system".

--- src/hooks/useTheme.tsx
+++ src/hooks/useTheme.tsx
@@ -1,18 +1,41 @@
 import { createEffect, createSignal } from "solid-js";
 
-export const useTheme = () => {
-  const preferredTheme = window.matchMedia("(prefers-color-scheme: dark)")
-    .matches
-    ? "dark"
-    : "light";
+const applySystemTheme = (e: MediaQueryListEvent) => {
+  e.matches
+    ? document.documentElement.classList.add("dark")
+    : document.documentElement.removeAttribute("class");
+};
+const media = window.matchMedia("(prefers-color-scheme: dark)");
 
-  const [theme, setTheme] = createSignal(
-    localStorage.getItem("theme") || preferredTheme
-  );
+export const useTheme = () => {
+  const [theme, setTheme] = createSignal(localStorage.getItem("theme"));
+
+  const HTML = document.documentElement;
 
   createEffect(() => {
-    document.documentElement.className = theme();
-    localStorage.setItem("theme", theme());
+    const _theme = theme();
+
+    if (!_theme) localStorage.setItem("theme", "light");
+    else {
+      if (_theme == "light") HTML.removeAttribute("class");
+      else if (_theme == "dark") HTML.classList.add("dark");
+      else {
+        media.matches && HTML.classList.add("dark");
+
+        try {
+          media.addEventListener("change", applySystemTheme);
+        } catch (e1) {
+          try {
+            media.addListener(applySystemTheme);
+          } catch (e2) {
+            console.error(e2);
+          }
+        }
+      }
+
+      localStorage.setItem("theme", _theme);
+    }
   });
+
   return { theme, setTheme };
 };
