ffb9d729a8fb4a7fa30a4fee54403e8828cc474a
Yael <srjean21@gmail.com>
2025-01-27 13:22:16 -0400

    (add) Se añadió un header para todas las secciones.

---
+++ src/components/header.tsx
@@ -0,0 +1,16 @@
+import * as Icons from "../icons/header";
+import { setAsideOpen } from "./aside";
+const Header = () => {
+  return (
+    <header class="sticky top-0 flex gap-4 items-center py-2 px-3 border-b-1 div-border">
+      <button aria-label="ver menú" onClick={() => setAsideOpen(true)}>
+        <Icons.Menu />
+      </button>
+      <h1 class="font-bold text-2xl">Registro Comunal</h1>
+      <button aria-label="buscar" class="ml-auto">
+        <Icons.Search />
+      </button>
+    </header>
+  );
+};
+export default Header;
