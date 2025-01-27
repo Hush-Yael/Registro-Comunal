6de8052ee29d470fadde3eb28f3c861a460ab48d
Yael <srjean21@gmail.com>
2025-01-27 13:29:44 -0400

    (adds) Se añadió un header para todas las secciones y un slider para navegar en la app.

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,26 @@
 import "./App.css";
-import { Router, RouteSectionProps } from "@solidjs/router";
+import { Route, Router, RouteSectionProps } from "@solidjs/router";
+import Header from "./components/header";
+import Aside from "./components/aside";
 import { useTheme } from "./hooks/useTheme";
+import Settings from "./pages/settings";
 
 export const { theme, setTheme } = useTheme();
 
 function App() {
-  const layout = (props: RouteSectionProps) => <>{props.children}</>;
+  const layout = (props: RouteSectionProps) => (
+    <>
+      <Header />
+      <Aside />
+      {props.children}
+    </>
+  );
 
-  return <Router root={layout}></Router>;
+  return (
+    <Router root={layout}>
+      <Route path="/configuracion" component={Settings} />
+    </Router>
+  );
 }
 
 export default App;
