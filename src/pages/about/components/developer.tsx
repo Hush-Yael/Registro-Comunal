a5badaeafbbba1b2a8ad78c23496fc8a8a906f79
Yael <srjean21@gmail.com>
2025-01-28 15:15:06 -0400

    (add) Se añadió este componente, para mostrar un imagen y descripción de cada desarrollador.

---
+++ src/pages/about/components/developer.tsx
@@ -0,0 +1,53 @@
+import { Show } from "solid-js";
+import { Email, Person, Tel } from "../../../icons";
+
+type Props = {
+  role: "main" | "secondary";
+  name: string;
+  imgSrc?: string;
+};
+
+const Developer = (props: Props) => {
+  const imgDimensions =
+    props.role === "main" ? "h-[6em] !w-[6em]" : "h-[4em] !w-[4em]";
+
+  return (
+    <div class="grid grid-cols-[auto_1fr] gap-x-3.5 gap-y-2 gray-container-100">
+      {props.imgSrc ? (
+        <img
+          class={`rounded-full object-cover ${imgDimensions}`}
+          src={props.imgSrc}
+          alt="imagen de desarrollador"
+        />
+      ) : (
+        <Person class={imgDimensions} />
+      )}
+      <div class="flex flex-col justify-center  w-fit">
+        <span
+          class={`${props.role === "main" ? "text-2xl" : "text-lg"} font-bold`}
+        >
+          {props.name}
+        </span>
+        <span class={`${props.role === "secondary" ? "text-sm" : ""} fore`}>
+          {props.role === "main"
+            ? "Desarrollador principal"
+            : "Colaborador en el proyecto"}
+        </span>
+      </div>
+      <Show when={props.role === "main"}>
+        <div class="col-span-2 px-2">
+          <p>Para soporte o sugerencias, contactar a: </p>
+          <div class="flex flex-wrap gap-x-2 py-1 text-sky-600 dark:text-sky-400">
+            <a class="flex items-center gap-1" href="">
+              <Email class="h-[1.125em]" /> srjean21@gmail.com
+            </a>
+            <a class="flex items-center gap-0.5" href="">
+              <Tel class="h-[1.125em]" /> 0412-7734204
+            </a>
+          </div>
+        </div>
+      </Show>
+    </div>
+  );
+};
+export default Developer;
