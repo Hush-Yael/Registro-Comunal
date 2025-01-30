23cec8ab9adbb44e6816e65ec924503dfead41f4
Yael <srjean21@gmail.com>
2025-01-29 22:50:54 -0400

    (adds) Se añadieron las contantes sobre los datos de los individuos.

---
+++ src/constants/index.ts
@@ -0,0 +1,44 @@
+export const NATIONALITIES = {
+  ve: "venezolan@",
+  co: "colombian@",
+  ec: "ecutorian@",
+  pe: "peruan@",
+  ot: "otra",
+};
+
+export const SEXES = {
+  M: "masculino",
+  F: "femenino",
+};
+
+export const EDOS_CIVIL = [
+  "solter@",
+  "casad@",
+  "viud@",
+  "divorciad@",
+  "concubinad@",
+  "unión libre",
+  "otro",
+];
+
+export const NIVELES_ESTUDIOS = [
+  "primaria",
+  "secundaria",
+  "superior",
+  "posgrado",
+  "otro",
+];
+
+export const PARENTESCOS = [
+  "hij@",
+  "padre",
+  "madre",
+  "herman@",
+  "abuel@",
+  "niet@",
+  "espos@",
+  "ti@",
+  "sobrin@",
+  "prim@",
+  "otro",
+];
