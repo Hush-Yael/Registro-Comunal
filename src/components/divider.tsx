e6bb77c0495d9b915b38ce1c071bef9da881275c
Yael <srjean21@gmail.com>
2025-01-31 22:39:01 -0400

    (adds & changes) Ahora se pueden indicar variantes, actualmente solo existe la normal (sin variante) y la horizontal.

--- src/components/divider.tsx
+++ src/components/divider.tsx
@@ -1,3 +1,13 @@
-const Hr = () => <hr class="div-border" />;
+type HrProps = {
+  variant?: string;
+};
+
+const Hr = (props: HrProps) => (
+  <hr
+    class={`div-border ${
+      props.variant === "horizontal" ? "border-r-1 h-4" : ""
+    }`}
+  />
+);
 
 export default Hr;
