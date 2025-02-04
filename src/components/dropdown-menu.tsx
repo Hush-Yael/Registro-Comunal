4aa2862e93d987f13f97261886e81142d62063d8
Yael <srjean21@gmail.com>
2025-02-03 21:21:52 -0400

    (add) Se añadió este componente para mostrar menús.

---
+++ src/components/dropdown-menu.tsx
@@ -0,0 +1,47 @@
+import { DropdownMenu } from "@kobalte/core/dropdown-menu";
+import { JSX } from "solid-js";
+import { Check } from "../icons";
+import { MenuRadioItemProps } from "@kobalte/core/src/menu/menu-radio-item.jsx";
+import { MenuTriggerProps } from "@kobalte/core/src/menu/menu-trigger.jsx";
+
+type Props = {
+  children: JSX.Element;
+};
+
+export const Menu = (props: Props) => (
+  <DropdownMenu>{props.children}</DropdownMenu>
+);
+
+export const Trigger = (
+  props: Props & { class: string } & MenuTriggerProps
+) => <DropdownMenu.Trigger {...props}>{props.children}</DropdownMenu.Trigger>;
+
+export const MenuContent = (
+  props: Props & {
+    class: string;
+  }
+) => (
+  <DropdownMenu.Portal>
+    <DropdownMenu.Content class={props.class || "dialog-content mt-1"}>
+      {props.children}
+    </DropdownMenu.Content>
+  </DropdownMenu.Portal>
+);
+
+const Indicator = () => (
+  <DropdownMenu.ItemIndicator>
+    <Check class="h-[1.2em]" />
+  </DropdownMenu.ItemIndicator>
+);
+
+export const RadioItem = (props: Props & MenuRadioItemProps) => (
+  <DropdownMenu.RadioItem
+    class="flex items-center gap-1 aria-[checked=false]:pl-[1.45em]"
+    {...props}
+  >
+    <Indicator />
+    {props.children}
+  </DropdownMenu.RadioItem>
+);
+
+export default Menu;
