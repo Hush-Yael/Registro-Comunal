b95a754be4dc2d96bafb5955b088e52c8d47eaab
Yael <srjean21@gmail.com>
2025-02-03 21:19:49 -0400

    (changes) Se añadió !important a las clases de tamaño.

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -5,13 +5,13 @@ const Header = () => {
   return (
     <header class="sticky z-5 top-0 flex gap-4 items-center py-2 px-3 border-b-1 div-border white">
       <button aria-label="ver menú" onClick={() => setAsideOpen(true)}>
-        <Icons.Menu class="h-[24px]" />
+        <Icons.Menu class="!h-[24px]" />
       </button>
       <h1 class="font-bold text-2xl">
         <A href="/">Registro Comunal</A>
       </h1>
       <button aria-label="buscar" class="ml-auto">
-        <Icons.Search class="h-[24px]" />
+        <Icons.Search class="!h-[24px]" />
       </button>
     </header>
   );
