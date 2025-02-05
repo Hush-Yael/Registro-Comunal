1c42b579bc0832270b88e50372956b54025a88a9
Yael <srjean21@gmail.com>
2025-02-04 23:03:55 -0400

    (changes & adds) Ahora el botón de buscar muestra un modal.

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,6 +1,8 @@
 import { A } from "@solidjs/router";
 import * as Icons from "../icons/header";
 import { asideOpen, setAsideOpen } from "./aside";
+import Search from "./search";
+
 const Header = () => {
   return (
     <header class="sticky z-5 top-0 flex gap-4 items-center h-[var(--h-h)] p-3 border-b-1 div-border white">
@@ -17,9 +19,7 @@ const Header = () => {
       <h1 class="font-bold text-2xl">
         <A href="/">Registro Comunal</A>
       </h1>
-      <button aria-label="buscar" class="ml-auto">
-        <Icons.Search class="!h-[24px]" />
-      </button>
+      <Search />
     </header>
   );
 };
---
+++ src/components/search.tsx
@@ -0,0 +1,36 @@
+import Modal, { CloseBtn } from "./modal";
+import { Dialog } from "@kobalte/core/dialog";
+import { Search as SearchIcon } from "../icons/header";
+import { createSignal } from "solid-js";
+
+const Search = () => {
+  const [query, setQuery] = createSignal("");
+
+  return (
+    <Modal
+      trigger={
+        <Dialog.Trigger class="ml-auto" aria-label="buscar">
+          <SearchIcon class="!h-[24px]" />
+        </Dialog.Trigger>
+      }
+      alert
+      class="!w-[90vw] h-[90%] !p-0"
+      contentClass="!p-0"
+    >
+      <div class="flex items-center gap-3 border-b-1 div-border">
+        <label class="flex items-center flex-1 py-2 " for="search">
+          <SearchIcon class="mx-3" />
+          <input
+            id="search"
+            class="w-full focus:outline-0"
+            placeholder="Buscar en los registros..."
+            value={query()}
+            onChange={(e) => setQuery(e.target.value.trim())}
+          />
+        </label>
+        <CloseBtn class="mr-3" />
+      </div>
+    </Modal>
+  );
+};
+export default Search;
