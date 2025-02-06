4137274d08938b6874e7cff524a67085009bc70f
Yael <srjean21@gmail.com>
2025-02-06 18:31:29 -0400

    (adds) Se añadieron funciones para mostrar algunos datos de manera especial.

---
+++ src/lib/data.ts
@@ -0,0 +1,29 @@
+import { ComunalRecord, HomePath } from "../types/form";
+import { plural } from "./utils";
+
+export const cedula = (tel: number | "") =>
+  tel ? tel.toLocaleString("es") : "Desconocida";
+
+export const tel = (tel: string) =>
+  tel ? tel.replace(/^(04\d{2})/, " $1-") : "Desconocido";
+
+export const parseDir = (calle: HomePath, avenida: HomePath) => {
+  const calles = calle.split("-"),
+    avenidas = avenida.split("-");
+
+  return `
+  ${plural("avenida", avenidas.length)} ${avenidas.join(" y ")},
+    ${plural("calle", calles.length)} ${calles.join(" y ")}
+  `;
+};
+
+export const getTotalGas = (record: ComunalRecord) => {
+  const nums = [
+    record.gas["10kg"],
+    record.gas["18kg"],
+    record.gas["27kg"],
+    record.gas["43kg"],
+  ];
+
+  return nums.reduce((p, curr) => p + (curr || 0), 0) || "";
+};
