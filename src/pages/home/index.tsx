9dc1611466d6a2e02a3e76155aa66de0b2d1492c
Yael <srjean21@gmail.com>
2025-02-07 19:44:47 -0400

    (changes) Ya no se va a realizar una guía de uso.

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,25 +1,20 @@
 import DescLink from "../../components/desc-link";
-import { Book, Paper } from "../../icons/home";
+import { Paper } from "../../icons/home";
 
 const Home = () => {
   return (
-    <main class="flex flex-col flex-1 gap-5 p-3 md:p-5">
-      <ul class="flex flex-col gap-2 min-[550px]:flex-row">
-        <DescLink
-          href="/guia-de-uso"
-          linkChildren={
-            <>
-              <span class="flex items-center gap-2">
-                <Book />
-                Ver guía de uso
-              </span>
-            </>
-          }
-        >
-          <p class="text-sm fore">
-            Conoce las funciones y cómo hacer uso correcto de esta aplicación.
-          </p>
-        </DescLink>
+    <main class="flex flex-col flex-1 gap-2 p-3 md:p-5">
+      <div class="flex flex-col gap-2">
+        <figure class="w-fit">
+          <figcaption class="fore">
+            <i>Croquis del sector</i>
+          </figcaption>
+          <img
+            src="/croquis.png"
+            alt="imagen del croquis del sector"
+            class="max-h-[500px] rounded-xl bg-white"
+          />
+        </figure>
         <DescLink
           href="/referencia-historica"
           linkChildren={
@@ -35,17 +30,7 @@ const Home = () => {
             Ver información acerca de la historia del sector y sus habitantes.
           </p>
         </DescLink>
-      </ul>
-      <figure class="w-fit">
-        <img
-          src="/croquis.png"
-          alt="imagen del croquis del sector"
-          class="max-h-[500px] rounded-xl bg-white"
-        />
-        <figcaption class="text-right fore">
-          <i>Croquis del sector</i>
-        </figcaption>
-      </figure>
+      </div>
     </main>
   );
 };
