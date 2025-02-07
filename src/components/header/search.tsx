68e979260fe5edb300f215801dea571974c46bd4
Yael <srjean21@gmail.com>
2025-02-06 20:42:40 -0400

    (changes & adds) Se movieron los componentes relacionados al input de búsqueda a su propia archivo.

--- src/components/header/search.tsx
+++ src/components/header/search.tsx
@@ -1,11 +1,10 @@
 import Modal, { CloseBtn } from "../modal";
 import { Dialog } from "@kobalte/core/dialog";
+import SearchInput from "../search";
 import { Search as SearchIcon } from "../../icons/header";
 import { createSignal } from "solid-js";
 
 const Search = () => {
-  const [query, setQuery] = createSignal("");
-
   return (
     <Modal
       trigger={
@@ -18,17 +17,12 @@ const Search = () => {
       contentClass="!p-0"
     >
       <div class="flex items-center gap-3 border-b-1 div-border">
-        <label class="flex items-center flex-1 py-2 " for="search">
-          <SearchIcon class="mx-3" />
-          <input
-            id="search"
-            class="w-full focus:outline-0"
-            placeholder="Buscar en los registros..."
-            value={query()}
-            onChange={(e) => setQuery(e.target.value.trim())}
-            autocomplete="off"
-          />
-        </label>
+        <SearchInput
+          id="search"
+          class="w-full focus:outline-0"
+          placeholder="Buscar en los registros..."
+          autocomplete="off"
+        />
         <CloseBtn class="mr-3" />
       </div>
     </Modal>
---
+++ src/components/search.tsx
@@ -0,0 +1,34 @@
+import { createSignal, JSX } from "solid-js";
+import { Search as SearchIcon } from "../icons/header";
+import Loader from "./loader";
+
+type SearchProps = Omit<JSX.IntrinsicElements["input"], "onInput"> & {
+  onInput: (value: string) => void;
+  debounce?: number;
+};
+
+const Search = (props: SearchProps) => {
+  let timeout: number;
+  const [searching, setSearching] = createSignal(false);
+
+  return (
+    <label class="flex items-center flex-1 py-2 " for="search">
+      <SearchIcon class="mx-3" />
+      <input
+        {...props}
+        onInput={(e) => {
+          clearTimeout(timeout);
+          const value = e.target.value.trim();
+          if (value)
+            timeout = setTimeout(async () => {
+              setSearching(true);
+              await props.onInput(value);
+              setSearching(false);
+            }, props.debounce);
+        }}
+      />
+      <Loader active={searching()} s={20} dt={0.2} />
+    </label>
+  );
+};
+export default Search;
