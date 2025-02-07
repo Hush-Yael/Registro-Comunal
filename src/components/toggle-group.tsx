379df5510dd3bb267d8ea7e3d2fe645abc452cf5
Yael <srjean21@gmail.com>
2025-02-06 22:56:42 -0400

    (add) Se añadió este componente para activar estados.

---
+++ src/components/toggle-group.tsx
@@ -0,0 +1,42 @@
+import {
+  ToggleGroup as Group,
+  ToggleGroupRootProps,
+} from "@kobalte/core/toggle-group";
+import { createEffect, createSignal, For, JSX } from "solid-js";
+
+type Option = { label: JSX.Element; value: string };
+type ToggleGroupProps = ToggleGroupRootProps & {
+  class?: string;
+  options: string[] | Option[];
+  onChange?: (value: string | string[]) => void;
+};
+
+const ToggleGroup = (props: ToggleGroupProps) => {
+  const [value, setValue] = createSignal<string[]>([]);
+
+  createEffect(() => {
+    props.onChange && props.onChange(value() as string & string[]);
+  });
+
+  return (
+    // @ts-expect-error
+    <Group
+      {...props}
+      class={`flex flex-wrap gap-2 ${props.class || ""}`}
+      value={value()}
+      onChange={setValue}
+    >
+      <For each={props.options}>
+        {(option) => (
+          <Group.Item
+            class="px-3 rounded-2xl border div-border aria-pressed:!border-neutral-900 dark:aria-pressed:!border-white not-aria-pressed:text-neutral-600 dark:not-aria-pressed:text-neutral-400 aria-pressed:bg-neutral-900 aria-pressed:text-white dark:aria-pressed:bg-white dark:aria-pressed:text-neutral-900"
+            value={typeof option === "string" ? option : option.value}
+          >
+            {typeof option === "string" ? option : option.label}
+          </Group.Item>
+        )}
+      </For>
+    </Group>
+  );
+};
+export default ToggleGroup;
