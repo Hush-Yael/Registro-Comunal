dd13fd11d76c4f3471af250116b14f7b93c859db
Yael <srjean21@gmail.com>
2025-02-08 14:05:39 -0400

    (add) Se añadió una nueva variante: warning.
    (fixes & adds) Ahora el título es bloque y tiene fuente en 500.

--- src/components/alert.tsx
+++ src/components/alert.tsx
@@ -1,10 +1,10 @@
 import { Match, Show, Switch } from "solid-js";
 import { JSX } from "solid-js";
-import { Check } from "../icons";
+import { Check, Alert as AlertIcon } from "../icons";
 
 type AlertProps = {
   class?: string;
-  variant: "success" | "alert" | "info" | "error";
+  variant: "success" | "alert" | "warning" | "info" | "error";
   title?: JSX.Element;
   children: JSX.Element;
 };
@@ -14,6 +14,12 @@ const Alert = (props: AlertProps) => {
     alert: `
       bg-[hsl(49_65%_80%)]
       text-[hsl(49_98%_20%)]
+    `,
+    warning: `
+      border border-[hsl(0_50%_50%)]
+      text-[hsl(0_80%_50%)]
+      dark:border-[hsl(0_100%_60%)]
+      dark:text-[hsl(0_100%_85%)]
     `,
     success: `
       bg-[hsl(135_65%_80%)]
@@ -34,18 +40,16 @@ const Alert = (props: AlertProps) => {
             <Check />
           </Match>
           <Match when={props.variant === "alert"}>
-            <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-              <g>
-                <path d="M0 0h24v24H0z" fill="none" />
-                <path d="M12.866 3l9.526 16.5a1 1 0 0 1-.866 1.5H2.474a1 1 0 0 1-.866-1.5L11.134 3a1 1 0 0 1 1.732 0zM11 16v2h2v-2h-2zm0-7v5h2V9h-2z" />
-              </g>
-            </svg>
+            <AlertIcon />
+          </Match>
+          <Match when={props.variant === "warning"}>
+            <AlertIcon />
           </Match>
         </Switch>
       </span>
       <span class="content">
         <Show when={props.title}>
-          <span class="title db fw500">{props.title}</span>
+          <span class="block font-[500]">{props.title}</span>
         </Show>
         <span class="description">{props.children}</span>
       </span>
