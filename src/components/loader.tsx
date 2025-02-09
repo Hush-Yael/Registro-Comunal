5699ae2ae667472f5137937f114a073e05747482
Yael <srjean21@gmail.com>
2025-02-09 14:12:54 -0400

    (adds & changes) Ahora el loader tiene un wrapper y se puede añadir texto al mismo.

--- src/components/loader.tsx
+++ src/components/loader.tsx
@@ -1,29 +1,34 @@
 import "./styles/loader.css";
-import { Show } from "solid-js";
+import { JSX, Show } from "solid-js";
 
 const Loader = (props: {
   class?: string;
   active: boolean;
   s?: number;
   dt?: number;
+  wrapperClass?: string;
+  children?: JSX.Element;
 }) => {
   return (
     <Show when={props.active}>
-      <div
-        class={`loader ${props.class || ""}`}
-        style={{
-          "--s": `${props.s || 30}px`,
-          "--dot-s": `calc(var(--s) * ${props.dt || 0.17})`,
-        }}
-        role="status"
-        aria-label="cargando"
-      >
-        <div class="dot" />
-        <div class="dot" />
-        <div class="dot" />
-        <div class="dot" />
-        <div class="dot" />
-        <div class="dot" />
+      <div class={`flex flex-col gap-2 w-fit h-fit ${props.wrapperClass}`}>
+        <div
+          class={`loader ${props.class || ""}`}
+          style={{
+            "--s": `${props.s || 30}px`,
+            "--dot-s": `calc(var(--s) * ${props.dt || 0.17})`,
+          }}
+          role="status"
+          aria-label="cargando"
+        >
+          <div class="dot" />
+          <div class="dot" />
+          <div class="dot" />
+          <div class="dot" />
+          <div class="dot" />
+          <div class="dot" />
+        </div>
+        {props.children}
       </div>
     </Show>
   );
