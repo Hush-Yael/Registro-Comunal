4bb82301f79aa0226d28aa8adb1336b1c0a3b7fe
Yael <srjean21@gmail.com>
2025-02-09 13:55:41 -0400

    (changes) Ahora se usa la debida función para mostrar la cédula con puntos.

--- src/pages/records/tabs/homes.tsx
+++ src/pages/records/tabs/homes.tsx
@@ -2,6 +2,7 @@ import { For } from "solid-js";
 import { Row, Table, Thead } from "../../../components/table";
 import { ComunalRecord } from "../../../types/form";
 import { parseDir } from "../../../lib/data";
+import { cedula as Cedula } from "../../../lib/data";
 
 const Homes = (props: {
   records: (ComunalRecord["home"] & { cedula: number })[];
@@ -17,7 +18,7 @@ const Homes = (props: {
       <For each={props.records}>
         {({ cedula, numCasa, calle, avenida, referencia }) => (
           <Row>
-            <td>{cedula.toLocaleString()}</td>
+            <td>{Cedula(cedula)}</td>
             <td>{numCasa}</td>
             <td class="first-letter:uppercase">{parseDir(calle, avenida)}</td>
             <td>{referencia}</td>
--- src/pages/records/tabs/jefes.tsx
+++ src/pages/records/tabs/jefes.tsx
@@ -2,6 +2,7 @@ import { For } from "solid-js";
 import { ComunalRecord } from "../../../types/form";
 import { Row, Table, Thead } from "../../../components/table";
 import { A } from "@solidjs/router";
+import { cedula as Cedula } from "../../../lib/data";
 
 const Jefes = (props: { records: ComunalRecord["jefe"][] }) => (
   <Table>
@@ -17,7 +18,7 @@ const Jefes = (props: { records: ComunalRecord["jefe"][] }) => (
           <Row>
             <td>{i() + 1}</td>
             <td class="min-w-max">
-              <A href={`/jefe/${cedula}`}>{cedula.toLocaleString()}</A>
+              <A href={`/jefe/${cedula}`}>{Cedula(cedula)}</A>
             </td>
             <td class="min-w-max">{nombres}</td>
             <td class="min-w-max">{apellidos}</td>
