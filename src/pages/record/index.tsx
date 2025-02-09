a654bc587b9dc72fa868a27650012213da234540
Yael <srjean21@gmail.com>
2025-02-09 14:10:51 -0400

    (changes) Se cambió el loader.
    (changes) Ahora no se devuelve null al no encontrar resultados, por lo que se cambió cómo se considera que no hay registros.
    
    (adds & changes) Ahora no se redirecciona al no haber registros, sino que se añadió un mensaje que indica lo anterior.

--- src/pages/record/index.tsx
+++ src/pages/record/index.tsx
@@ -1,43 +1,56 @@
-import { createResource, Show, Suspense } from "solid-js";
+import { createResource, createSignal, Show, Suspense } from "solid-js";
 import { getRecord } from "../../lib/db";
-import { Navigate, useParams } from "@solidjs/router";
+import { useParams } from "@solidjs/router";
 import Jefe from "./components/jefe";
 import { ComunalRecord } from "../../types/form";
 import Home from "./components/home";
 import Family from "./components/family";
 import Programs from "./components/programs";
+import Loader from "../../components/loader";
+import NotFound from "./components/not-found";
 
 const Record = () => {
   const { cedula } = useParams();
+  const [empty, setEmpty] = createSignal(false);
 
   const [data] = createResource(async () => {
     const data = await getRecord(parseInt(cedula));
-
-    const empty = Object.values(data).every((_) => _ === undefined);
-    if (empty) return null;
-
+    setEmpty(
+      !data.family.length &&
+        Object.values({ ...data, family: undefined }).every(
+          (_) => _ === undefined
+        )
+    );
     return data;
   });
 
   return (
-    <main class="p-4">
-      <Suspense fallback={<div>Loading...</div>}>
-        <Show when={!data() === null}>
-          <Navigate href="/" />
-        </Show>
+    <main class="relative p-4 flex-1">
+      <Suspense
+        fallback={
+          <Loader wrapperClass="absolute inset-0 m-auto" active s={60}>
+            Cargando...
+          </Loader>
+        }
+      >
         <Show when={data()}>
-          <div class="flex flex-col gap-5 *:max-w-[450px] *:w-full *:m-auto">
-            <Jefe data={(data() as ComunalRecord).jefe} />
-            <Home data={(data() as ComunalRecord).home} />
-            <Family data={(data() as ComunalRecord).family} />
-            <Programs
-              data={{
-                carnet: (data() as ComunalRecord).carnet,
-                clap: (data() as ComunalRecord).clap,
-                gas: (data() as ComunalRecord).gas,
-              }}
-            />
-          </div>
+          <Show when={empty()}>
+            <NotFound cedula={cedula} />
+          </Show>
+          <Show when={!empty()}>
+            <div class="flex flex-col gap-5 *:max-w-[450px] *:w-full *:m-auto">
+              <Jefe data={(data() as ComunalRecord).jefe} />
+              <Home data={(data() as ComunalRecord).home} />
+              <Family data={(data() as ComunalRecord).family} />
+              <Programs
+                data={{
+                  carnet: (data() as ComunalRecord).carnet,
+                  clap: (data() as ComunalRecord).clap,
+                  gas: (data() as ComunalRecord).gas,
+                }}
+              />
+            </div>
+          </Show>
         </Show>
       </Suspense>
     </main>
