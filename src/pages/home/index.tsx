098516fbda78d94f9d29dc6927aedb3ec27b1658
Yael <srjean21@gmail.com>
2025-02-10 17:06:58 -0400

    (adds) Se añadió una sección para la base de datos.
    (changes) Se movieron los elementos de la información sobre la comunidad a su propio archivo.

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,36 +1,11 @@
-import DescLink from "../../components/desc-link";
-import { Paper } from "../../icons/home";
+import DB from "./components/db";
+import Info from "./components/info";
 
 const Home = () => {
   return (
-    <main class="flex flex-col flex-1 gap-2 p-3 md:p-5">
-      <div class="flex flex-col gap-2">
-        <figure class="w-fit">
-          <figcaption class="fore">
-            <i>Croquis del sector</i>
-          </figcaption>
-          <img
-            src="/croquis.png"
-            alt="imagen del croquis del sector"
-            class="max-h-[500px] rounded-xl bg-white"
-          />
-        </figure>
-        <DescLink
-          href="/referencia-historica"
-          linkChildren={
-            <>
-              <span class="flex items-center gap-2">
-                <Paper />
-                Ver referencia histórica
-              </span>
-            </>
-          }
-        >
-          <p class="text-sm fore">
-            Ver información acerca de la historia del sector y sus habitantes.
-          </p>
-        </DescLink>
-      </div>
+    <main class="flex flex-col flex-1 gap-8 p-3 md:p-5">
+      <Info />
+      <DB />
     </main>
   );
 };
