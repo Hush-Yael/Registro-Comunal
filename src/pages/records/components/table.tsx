eebc76b193a4599520eb6ca24ae529e32ef384fb
Yael <srjean21@gmail.com>
2025-02-09 22:31:35 -0400

    (changes & fix) Ahora el mensaje de "no registros" se expande de acuerdo a la cantidad de columnas.

--- src/pages/records/components/table.tsx
+++ src/pages/records/components/table.tsx
@@ -1,4 +1,4 @@
-import { Show, For, JSX, Accessor } from "solid-js";
+import { Show, For, JSX, Accessor, children } from "solid-js";
 import { NoFile } from "../../../icons";
 import { ComunalRecord } from "../../../types/form";
 import { Table as TABLE, Thead } from "../../../components/table";
@@ -27,7 +27,14 @@ export function Table<Key extends keyof ComunalRecord>(props: TableProps<Key>) {
       <tbody class={props.tbodyClass}>
         <Show when={!props.records.length}>
           <tr>
-            <td class="pt-2" colSpan={4}>
+            <td
+              class="pt-2"
+              colSpan={
+                Array.isArray(props.columns)
+                  ? props.columns.length
+                  : children(() => props.columns).toArray().length
+              }
+            >
               <span class="flex items-center justify-center gap-1.5">
                 <NoFile class="text-red-700 dark:text-red-400" /> No hay
                 registros
