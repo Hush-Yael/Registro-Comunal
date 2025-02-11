dde187b89794ba51146519bd3a7233ed6032326d
Yael <srjean21@gmail.com>
2025-02-10 23:14:38 -0400

    (change) Se cambiaron las dimensiones de las imágenes.
    (changes) Se cambió la disposición de los elementos.
    
    (change) Ahora no se muestra la descripción de "Colaborador en el proyecto".
    
    (changes) Se quitó la información de contacto.

--- src/pages/about/components/developer.tsx
+++ src/pages/about/components/developer.tsx
@@ -1,5 +1,4 @@
-import { Show } from "solid-js";
-import { Email, Person, Tel } from "../../../icons";
+import { Person } from "../../../icons";
 
 type Props = {
   role: "main" | "secondary";
@@ -8,10 +7,14 @@ type Props = {
 };
 
 const Developer = (props: Props) => {
-  const imgDimensions = props.role === "main" ? "size-[6em]" : "!size-[4em]";
+  const imgDimensions = props.role === "main" ? "size-[10em]" : "!size-[1.5em]";
 
   return (
-    <div class="grid grid-cols-[auto_1fr] gap-x-3.5 gap-y-2 gray-container-100">
+    <div
+      class={`flex ${
+        props.role === "main" ? "flex-col" : ""
+      } items-center gap-2`}
+    >
       {props.imgSrc ? (
         <img
           class={`rounded-full object-cover ${imgDimensions}`}
@@ -21,31 +24,16 @@ const Developer = (props: Props) => {
       ) : (
         <Person class={imgDimensions} />
       )}
-      <div class="flex flex-col justify-center  w-fit">
+      <p class="flex flex-col justify-center w-fit">
         <span
           class={`${props.role === "main" ? "text-2xl" : "text-lg"} font-bold`}
         >
           {props.name}
         </span>
-        <span class={`${props.role === "secondary" ? "text-sm" : ""} fore`}>
-          {props.role === "main"
-            ? "Desarrollador principal"
-            : "Colaborador en el proyecto"}
-        </span>
-      </div>
-      <Show when={props.role === "main"}>
-        <div class="col-span-2 px-2">
-          <p>Para soporte o sugerencias, contactar a: </p>
-          <div class="flex flex-wrap gap-x-2 py-1 text-sky-600 dark:text-sky-400">
-            <a class="flex items-center gap-1" href="mailto:srjean21@gmail.com">
-              <Email /> srjean21@gmail.com
-            </a>
-            <a class="flex items-center gap-0.5" href="tel:+584127734204">
-              <Tel /> 0412-7734204
-            </a>
-          </div>
-        </div>
-      </Show>
+        {props.role === "main" && (
+          <span class="fore">Desarrollador principal</span>
+        )}
+      </p>
     </div>
   );
 };
