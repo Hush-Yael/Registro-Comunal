a30e1a51bee982816fd76b2784327fe939f540af
Yael <srjean21@gmail.com>
2025-02-10 23:21:06 -0400

    (adds) Se añadió un breakpoint que cambia diseño a dos columnas.
    (fix) Se intercambiaron de lugar las pestañas.

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,11 +1,16 @@
+import { useMedia } from "../../hooks/useMedia";
 import DB from "./components/db";
 import Info from "./components/info";
 
+export const breakpoint = useMedia(
+  "(min-width: 1024px) and (max-width: 1199px)"
+);
+
 const Home = () => {
   return (
-    <main class="flex flex-col flex-1 gap-8 p-3 md:p-5">
-      <Info />
+    <main class="flex flex-col flex-1 gap-8 p-3 md:p-5 min-[1200px]:grid min-[1200px]:grid-cols-2">
       <DB />
+      <Info />
     </main>
   );
 };
