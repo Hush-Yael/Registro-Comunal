09dc44422c72d733c9059dd337330365b3109a7d
Yael <srjean21@gmail.com>
2025-02-11 23:18:51 -0400

    (changes) La función que devuelve el historial ahora es síncrona.

--- src/components/header/search/history.tsx
+++ src/components/header/search/history.tsx
@@ -1,12 +1,9 @@
-import { createResource, For, Show } from "solid-js";
+import { Show } from "solid-js";
 import { Hour } from "../../../icons/header";
-import Loader from "../../loader";
 import { getHistory } from "../../../lib/db";
 
 const History = () => {
-  const [history] = createResource(async () => {
-    return await getHistory();
-  });
+  const history = getHistory();
 
   return (
     <div class="relative div-y-neutral h-full max-h-full overflow-auto *:p-3">
@@ -15,17 +12,12 @@ const History = () => {
         Búsquedas recientes
       </h2>
       <Show
-        when={history() !== undefined}
-        fallback={<Loader class="!absolute inset-0 m-auto" s={50} active />}
+        when={history.length}
+        fallback={
+          <p class="fore text-lg text-center">No hay búsquedas recientes</p>
+        }
       >
-        <Show
-          when={history()}
-          fallback={
-            <p class="fore text-lg text-center">No hay búsquedas recientes</p>
-          }
-        >
-          <ul class="div-y-neutral !py-0"></ul>
-        </Show>
+        <ul class="div-y-neutral !py-0"></ul>
       </Show>
     </div>
   );
