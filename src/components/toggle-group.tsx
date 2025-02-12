87ae64e08006216798e2600e129e96a0c5900f57
Yael <srjean21@gmail.com>
2025-02-12 15:26:11 -0400

    (adds) Ahora los botones son flex.

--- src/components/toggle-group.tsx
+++ src/components/toggle-group.tsx
@@ -34,7 +34,7 @@ const ToggleGroup = (props: ToggleGroupProps) => {
       <For each={props.options}>
         {(option) => (
           <Group.Item
-            class="px-3 rounded-2xl border div-border aria-pressed:!border-neutral-900 dark:aria-pressed:!border-white not-aria-pressed:text-neutral-600 dark:not-aria-pressed:text-neutral-400 aria-pressed:bg-neutral-900 aria-pressed:text-white dark:aria-pressed:bg-white dark:aria-pressed:text-neutral-900"
+            class="flex items-center justify-center gap-2 px-3 rounded-2xl border div-border aria-pressed:!border-neutral-900 dark:aria-pressed:!border-white not-aria-pressed:text-neutral-600 dark:not-aria-pressed:text-neutral-400 aria-pressed:bg-neutral-900 aria-pressed:text-white dark:aria-pressed:bg-white dark:aria-pressed:text-neutral-900"
             value={typeof option === "string" ? option : option.value}
           >
             {typeof option === "string" ? option : option.label}
