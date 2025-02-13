f7593385455f1ed629492dcb7626adab70630c85
Yael <srjean21@gmail.com>
2025-02-12 22:29:44 -0400

    (changes) Se extrajeron los datos a su propio componente y se exporta.

--- src/pages/record/components/home.tsx
+++ src/pages/record/components/home.tsx
@@ -4,6 +4,20 @@ import { ComunalRecord } from "../../../types/form";
 import { Home as HomeIcon } from "../../../icons/aside";
 import Data from "./data";
 
+export const HomeData = (props: { data: ComunalRecord["home"] }) => (
+  <>
+    <Data label="Número de casa">{props.data.numCasa || "Desconocido"}</Data>
+    <Data label="Dirección">
+      <span class="first-letter:uppercase">
+        {parseDir(props.data.calle, props.data.avenida)}
+      </span>
+    </Data>
+    <Data label="Referencias">
+      {props.data.referencia || "Sin referencias"}
+    </Data>
+  </>
+);
+
 const Home = (props: { data: ComunalRecord["home"] }) => {
   return (
     <section>
@@ -13,17 +27,7 @@ const Home = (props: { data: ComunalRecord["home"] }) => {
       </SectionTitle>
       <article class="py-3 items-center gap-4">
         <div class="flex flex-wrap gap-x-4 gap-y-1 justify-between gray-container-100 !p-3">
-          <Data label="Número de casa">
-            {props.data.numCasa || "Desconocido"}
-          </Data>
-          <Data label="Dirección">
-            <span class="first-letter:uppercase">
-              {parseDir(props.data.calle, props.data.avenida)}
-            </span>
-          </Data>
-          <Data label="Referencias">
-            {props.data.referencia || "Sin referencias"}
-          </Data>
+          <HomeData data={props.data} />
         </div>
       </article>
     </section>
