1f31276d19b032e7c775c4a90a0cc821c83aea9e
Yael <srjean21@gmail.com>
2025-02-13 14:38:35 -0400

    (changes & adds) Ahora se muestra el prefijo en la cédula.

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -1,8 +1,10 @@
 import { ComunalRecord, HomePath } from "../types/form";
 import { plural } from "./utils";
 
-export const cedula = (data: number | "") =>
-  data ? data.toLocaleString("es") : "Desconocida";
+export const cedula = (data: number | "", venezolano = true) => {
+  const _cedula = data ? data.toLocaleString("es") : "Desconocida";
+  return _cedula ? `${venezolano ? "V" : "E"} - ${_cedula}` : "Desconocida";
+};
 
 export const tel = (tel: string) =>
   tel ? tel.replace(/^(04\d{2})/, " $1-") : "Desconocido";
