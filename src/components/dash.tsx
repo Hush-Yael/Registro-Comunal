e5bafe4404117b9d2e5802a3a3e6fab94b5085bf
Yael <srjean21@gmail.com>
2025-02-13 16:48:49 -0400

    (changes & adds) Se añadió un componente parra mostrar "—".

---
+++ src/components/dash.tsx
@@ -0,0 +1,5 @@
+const Dash = () => (
+  <span class="text-neutral-300 dark:text-neutral-600">—</span>
+);
+
+export default Dash;
--- src/components/edad.tsx
+++ src/components/edad.tsx
@@ -1,19 +1,19 @@
 import { Show } from "solid-js";
 import { yearsSinceDate } from "../lib/utils";
+import Dash from "./dash";
 
 type AgeProps = {
   date: string | undefined;
 };
 
-const Age = (props: AgeProps) => {
-  return (
-    <span class="flex gap-1">
-      {props.date || <i class="fore">Desconocida</i>}
-      <Show when={props.date}>
-        <span class="text-neutral-300 dark:text-neutral-600">—</span>
-        {yearsSinceDate(props.date as string)} años
-      </Show>
-    </span>
-  );
-};
+const Age = (props: AgeProps) => (
+  <span class="flex gap-1">
+    {props.date || <i class="fore">Desconocida</i>}
+    <Show when={props.date}>
+      <Dash />
+      {yearsSinceDate(props.date as string)} años
+    </Show>
+  </span>
+);
+
 export default Age;
