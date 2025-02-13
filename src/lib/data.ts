aec8012268e6bea92bd57feed16ac66d943e3d53
Yael <srjean21@gmail.com>
2025-02-13 16:45:26 -0400

    (add) Ahora se puede indicar no mostrar el prefijo en la cédula.

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -1,9 +1,16 @@
 import { ComunalRecord, HomePath } from "../types/form";
 import { plural } from "./utils";
 
-export const cedula = (data: number | "", venezolano = true) => {
+export const cedula = (
+  data: number | "",
+  venezolano: boolean | null = true
+) => {
   const _cedula = data ? data.toLocaleString("es") : "Desconocida";
-  return _cedula ? `${venezolano ? "V" : "E"} - ${_cedula}` : "Desconocida";
+  return _cedula
+    ? venezolano !== null
+      ? `${venezolano ? "V" : "E"} - ${_cedula}`
+      : _cedula
+    : "Desconocida";
 };
 
 export const tel = (tel: string) => tel.replace(/^(04\d{2})/, " $1-");
