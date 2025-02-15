26a0b1064dea6fb9a80463baf9064809e01ef420
Yael <srjean21@gmail.com>
2025-02-15 13:44:16 -0400

    (changes & adds) Se creó un componente aparte que se encarga de mostrar las gráficas de torta o dona, para que no esté atado a las props relacionadas a las preguntas.

--- src/components/charts/question.tsx
+++ src/components/charts/question.tsx
@@ -1,40 +1,11 @@
-import {
-  Chart,
-  DoughnutController,
-  ArcElement,
-  Tooltip,
-  Legend,
-  Title,
-} from "chart.js";
-import { useTheme } from "../../hooks/useTheme";
-import { createEffect, onMount, Show } from "solid-js";
 import { QuestionLabel } from "../../types/form";
+import { RoundChart } from "./round";
 
-Chart.register(DoughnutController, Legend, Title, ArcElement, Tooltip);
-
-type ChartProps = {
+type QuestionChartProps = {
   title: string;
   data: [number, number, number];
   size?: number;
   onHide?: (legend: boolean | null | undefined) => void;
-};
-
-const { theme } = useTheme(),
-  media = window.matchMedia("(prefers-color-scheme: dark)");
-
-const matchTheme = (dark: boolean, chart: Chart) => {
-  const title = chart.options.plugins!.title!,
-    labels = chart.options.plugins!.legend!.labels;
-
-  if (dark) {
-    title.color = "white";
-    labels!.color = "white";
-  } else {
-    title.color = "black";
-    labels!.color = "black";
-  }
-
-  chart.update();
 };
 
 const parseLegend = (text: QuestionLabel) => {
@@ -48,107 +19,15 @@ const parseLegend = (text: QuestionLabel) => {
   }
 };
 
-export const QuestionChart = (props: ChartProps) => {
-  let c: HTMLCanvasElement;
-
-  onMount(() => {
-    if (c!) {
-      const chart = new Chart(c!, {
-        type: "doughnut",
-        data: {
-          labels: ["Sí", "No", "Desconocido"] as QuestionLabel[],
-          datasets: [
-            {
-              data: props.data,
-              backgroundColor: [
-                "hsl(130, 85%, 50%)",
-                "hsl(0, 100%, 50%)",
-                "hsl(30, 0%, 50%)",
-              ],
-            },
-          ],
-        },
-        options: {
-          layout: {
-            padding: 5,
-          },
-          plugins: {
-            title: {
-              display: true,
-              text: props.title,
-              font: {
-                family: "Afacad",
-                size: 19,
-              },
-            },
-            legend: {
-              position: "bottom",
-              labels: {
-                padding: 15,
-                font: { family: "Afacad", size: 15 },
-              },
-              onClick: function (_, legendItem, legend) {
-                const { index, hidden } = legendItem;
-                const shouldShowOnlyOne =
-                  hidden || legend.legendItems!.every((l) => !l.hidden);
-
-                for (let i = 0; i < legend.legendItems!.length; i++) {
-                  const l = legend.legendItems![i];
-                  if (!hidden) chart.toggleDataVisibility(l.index!);
-                  else if (!l.hidden || index === l.index)
-                    chart.toggleDataVisibility(l.index!);
-                }
-
-                if (!hidden) chart.toggleDataVisibility(index!);
-
-                chart.update();
-                if (props.onHide)
-                  props.onHide(
-                    shouldShowOnlyOne
-                      ? parseLegend(legendItem.text as QuestionLabel)
-                      : undefined
-                  );
-              },
-            },
-            tooltip: {
-              callbacks: {
-                label: function (context) {
-                  const currentValue = context.raw as number,
-                    //@ts-ignore
-                    total = context.chart._metasets[context.datasetIndex].total;
-
-                  const percentage = parseFloat(
-                    ((currentValue / total) * 100).toFixed(1)
-                  );
-
-                  return ` ${percentage}% — (${currentValue})`;
-                },
-              },
-            },
-          },
-        },
-      });
-
-      createEffect(() => {
-        if (theme() === "dark") matchTheme(true, chart);
-        else if (theme() === "light") matchTheme(false, chart);
-        else matchTheme(media.matches, chart);
-      });
-
-      if (theme() === "system") {
-        media.addEventListener("change", (e) => matchTheme(e.matches, chart));
-      }
-    }
-  });
-
-  return (
-    !props.data.every((d) => d === 0) && (
-      <canvas
-        ref={c!}
-        width={props.size || 200}
-        height={props.size || 200}
-        class="rounded-2xl bg-neutral-50 dark:bg-neutral-800 shadow-[0_0_3px_0_hsla(0,0%,0%,0.2)] dark:shadow-[0_0_0_1px] dark:shadow-neutral-700"
-      />
-    )
-  );
-};
+export const QuestionChart = (props: QuestionChartProps) => (
+  <RoundChart
+    title={props.title}
+    type="doughnut"
+    labels={["Sí", "No", "Desconocido"]}
+    colors={["hsl(130, 85%, 50%)", "hsl(0, 100%, 50%)", "hsl(30, 0%, 50%)"]}
+    size={props.size}
+    data={props.data}
+    parseLegend={parseLegend as (legend: string) => unknown}
+    onHide={props.onHide as (legend: unknown) => void}
+  />
+);
---
+++ src/components/charts/round.tsx
@@ -0,0 +1,152 @@
+import {
+  Chart,
+  PieController,
+  DoughnutController,
+  ArcElement,
+  Tooltip,
+  Legend,
+  Title,
+} from "chart.js";
+import { useTheme } from "../../hooks/useTheme";
+import { onMount, createEffect } from "solid-js";
+
+Chart.register(
+  DoughnutController,
+  PieController,
+  Legend,
+  Title,
+  ArcElement,
+  Tooltip
+);
+
+type ChartProps = {
+  type: "doughnut" | "pie";
+  title: string;
+  labels: string[];
+  colors: `hsl(${number}, ${number}%, ${number}%)`[];
+  data: number[];
+  size?: number;
+  parseLegend?: (legend: string) => unknown;
+  onHide?: (legend: unknown) => void;
+};
+
+const { theme } = useTheme(),
+  media = window.matchMedia("(prefers-color-scheme: dark)");
+
+const matchTheme = (dark: boolean, chart: Chart) => {
+  const title = chart.options.plugins!.title!,
+    labels = chart.options.plugins!.legend!.labels;
+
+  if (dark) {
+    title.color = "white";
+    labels!.color = "white";
+  } else {
+    title.color = "black";
+    labels!.color = "black";
+  }
+
+  chart.update();
+};
+
+export const RoundChart = (props: ChartProps) => {
+  let c: HTMLCanvasElement;
+
+  onMount(() => {
+    if (c!) {
+      const chart = new Chart(c!, {
+        type: props.type,
+        data: {
+          labels: props.labels,
+          datasets: [
+            {
+              data: props.data,
+              backgroundColor: props.colors,
+            },
+          ],
+        },
+        options: {
+          layout: {
+            padding: 5,
+          },
+          plugins: {
+            title: {
+              display: true,
+              text: props.title,
+              font: {
+                family: "Afacad",
+                size: 19,
+              },
+            },
+            legend: {
+              position: "bottom",
+              labels: {
+                padding: 15,
+                font: { family: "Afacad", size: 15 },
+              },
+              onClick: function (_, legendItem, legend) {
+                const { index, hidden } = legendItem;
+                const shouldShowOnlyOne =
+                  hidden || legend.legendItems!.every((l) => !l.hidden);
+
+                for (let i = 0; i < legend.legendItems!.length; i++) {
+                  const l = legend.legendItems![i];
+                  if (!hidden) chart.toggleDataVisibility(l.index!);
+                  else if (!l.hidden || index === l.index)
+                    chart.toggleDataVisibility(l.index!);
+                }
+
+                if (!hidden) chart.toggleDataVisibility(index!);
+
+                chart.update();
+                if (props.onHide)
+                  props.onHide(
+                    shouldShowOnlyOne
+                      ? props.parseLegend
+                        ? props.parseLegend(legendItem.text)
+                        : legendItem.text
+                      : undefined
+                  );
+              },
+            },
+            tooltip: {
+              callbacks: {
+                label: function (context) {
+                  const currentValue = context.raw as number,
+                    //@ts-ignore
+                    total = context.chart._metasets[context.datasetIndex].total;
+
+                  const percentage = parseFloat(
+                    ((currentValue / total) * 100).toFixed(1)
+                  );
+
+                  return ` ${percentage}% — (${currentValue})`;
+                },
+              },
+            },
+          },
+        },
+      });
+
+      createEffect(() => {
+        if (theme() === "dark") matchTheme(true, chart);
+        else if (theme() === "light") matchTheme(false, chart);
+        else matchTheme(media.matches, chart);
+      });
+
+      if (theme() === "system") {
+        media.addEventListener("change", (e) => matchTheme(e.matches, chart));
+      }
+    }
+  });
+
+  return (
+    !props.data.every((d) => d === 0) && (
+      <canvas
+        ref={c!}
+        width={props.size || 200}
+        height={props.size || 200}
+        class="rounded-2xl bg-neutral-50 dark:bg-neutral-800 shadow-[0_0_3px_0_hsla(0,0%,0%,0.2)] dark:shadow-[0_0_0_1px] dark:shadow-neutral-700"
+      />
+    )
+  );
+};
