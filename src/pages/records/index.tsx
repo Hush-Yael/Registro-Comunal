2dccf81a389f964fdf7e644d5ea6d93402d82857
Yael <srjean21@gmail.com>
2025-02-16 13:41:18 -0400

    (changes) Se movieron las tabs al index.

--- src/pages/records/index.tsx
+++ src/pages/records/index.tsx
@@ -1,13 +1,63 @@
 import { createResource } from "solid-js";
-import Tabs from "./tabs";
 import { getRecords } from "../../lib/db";
+import { For, Show, Suspense } from "solid-js";
+import { Tabs } from "@kobalte/core/tabs";
+import Jefes from "./tabs/jefes";
+import Homes from "./tabs/homes";
+import Clap from "./tabs/clap";
+import Carnet from "./tabs/carnet";
+import Gas from "./tabs/gas";
+import Loader from "../../components/loader";
+import { RecordKey } from "../../types/form";
+
+const tabs = [
+  { label: "Jefes de hogar", value: "jefe", content: Jefes },
+  { label: "Viviendas", value: "vivienda", content: Homes },
+  { label: "CLAP", value: "clap", content: Clap },
+  { label: "Carnet", value: "carnet", content: Carnet },
+  { label: "Gas", value: "gas", content: Gas },
+];
 
 const Records = () => {
   const [records] = createResource(async () => await getRecords());
 
   return (
     <main class="p-2 px-3">
-      <Tabs records={records()} />
+      <section>
+        <Tabs class="tabs" data-list-pos="r">
+          <Tabs.List class="tab-list sticky top-0">
+            <For each={tabs}>
+              {(tab) => (
+                <Tabs.Trigger value={tab.value}>{tab.label}</Tabs.Trigger>
+              )}
+            </For>
+            <Tabs.Indicator class="tab-indicator" />
+          </Tabs.List>
+          <Suspense
+            fallback={
+              <Loader
+                wrapperClass="absolute top-0 bottom-0 left-0 right-0 m-auto"
+                s={80}
+                active
+              >
+                Cargando...
+              </Loader>
+            }
+          >
+            <Show when={records}>
+              <For each={tabs}>
+                {({ value, content }) => (
+                  <Tabs.Content value={value} class="flex flex-col gap-6 pt-1">
+                    {content({
+                      records: records[value as RecordKey],
+                    })}
+                  </Tabs.Content>
+                )}
+              </For>
+            </Show>
+          </Suspense>
+        </Tabs>
+      </section>
     </main>
   );
 };
