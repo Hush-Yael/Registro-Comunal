99dcb8edc9c94b5106a9fdc490cc6889354fc0fc
Yael <srjean21@gmail.com>
2025-02-15 20:41:59 -0400

    (changes) Ahora se pueden extender todas las clases.

--- src/components/table.tsx
+++ src/components/table.tsx
@@ -2,15 +2,20 @@ import { JSX } from "solid-js";
 
 type Props = {
   children: JSX.Element;
+  class?: string;
 };
 
 export const Table = (props: Props) => (
-  <table class="min-w-max px-1.5 border border-spacing-0 border-separate border-tools-table-outline gray-container-100">
+  <table
+    class={`min-w-max px-1.5 border border-spacing-0 border-separate border-tools-table-outline gray-container-100 ${
+      props.class || ""
+    }`}
+  >
     {props.children}
   </table>
 );
 
-export const Thead = (props: Props & { class?: string }) => (
+export const Thead = (props: Props) => (
   <thead>
     <tr
       class={`*:border-b-1 *:border-neutral-200 dark:*:border-neutral-700 *:p-1 *:px-3 *:text-left ${
@@ -23,7 +28,11 @@ export const Thead = (props: Props & { class?: string }) => (
 );
 
 export const Row = (props: Props) => (
-  <tr class="*:p-2 *:px-3 *:border-b-1 *:border-neutral-200 dark:*:border-neutral-700 last:*:border-0">
+  <tr
+    class={`*:p-2 *:px-3 *:border-b-1 *:border-neutral-200 dark:*:border-neutral-700 last:*:border-0 ${
+      props.class || ""
+    }`}
+  >
     {props.children}
   </tr>
 );
