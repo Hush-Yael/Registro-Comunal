89d7749ed8b3c1f8f47c4c6a55a3732d5e141a08
Yael <srjean21@gmail.com>
2025-02-17 13:41:37 -0400

    (changes & adds) Se añadió un wrapper para el canvas.

--- src/components/charts/question.tsx
+++ src/components/charts/question.tsx
@@ -4,21 +4,24 @@ export type QuestionChartMatch = 1 | 0 | null | undefined;
 
 type QuestionChartProps = Omit<ChartProps, "type" | "labels" | "onSelect"> & {
   data: [number, number, number];
+  class?: string;
   onSelect?: (legend: QuestionChartMatch) => void;
 };
 
 export const QuestionChart = (props: QuestionChartProps) => (
-  <RoundChart
-    title={props.title}
-    type="doughnut"
-    labels={[
-      { text: "Sí", match: 1 },
-      { text: "No", match: 0 },
-      { text: "Desconocido", match: null },
-    ]}
-    colors={["hsl(130, 85%, 50%)", "hsl(0, 100%, 50%)", "hsl(30, 0%, 50%)"]}
-    size={props.size}
-    data={props.data}
-    onSelect={props.onSelect as (legend: unknown) => void}
-  />
+  <div class={props.class}>
+    <RoundChart
+      title={props.title}
+      type="doughnut"
+      labels={[
+        { text: "Sí", match: 1 },
+        { text: "No", match: 0 },
+        { text: "Desconocido", match: null },
+      ]}
+      colors={["hsl(130, 85%, 50%)", "hsl(0, 100%, 50%)", "hsl(30, 0%, 50%)"]}
+      size={props.size}
+      data={props.data}
+      onSelect={props.onSelect as (legend: unknown) => void}
+    />
+  </div>
 );
