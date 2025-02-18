31b7957a4ff7f1166f79e07669996cceaefee87c
Yael <srjean21@gmail.com>
2025-02-18 17:05:54 -0400

    (add) Ahora se puede expandir la clase.

--- src/pages/record/components/data.tsx
+++ src/pages/record/components/data.tsx
@@ -1,9 +1,14 @@
 import { JSX } from "solid-js";
 
-type DataProps = { label: string; labelClass?: string; children: JSX.Element };
+type DataProps = {
+  class?: string;
+  label: string;
+  labelClass?: string;
+  children: JSX.Element;
+};
 
 const Data = (props: DataProps) => (
-  <p class="flex flex-col">
+  <p class={`flex flex-col ${props.class || ""}`}>
     <small class={`fore ${props.labelClass || ""}`}>{props.label}</small>
     {props.children}
   </p>
