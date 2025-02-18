96872a16009e99f51be7ffd17657b4800f5ddcd0
Yael <srjean21@gmail.com>
2025-02-17 22:16:11 -0400

    (changes) Se cambió el layout

--- src/pages/records/components/only-chart-wrapper.tsx
+++ src/pages/records/components/only-chart-wrapper.tsx
@@ -2,7 +2,9 @@ import { JSX } from "solid-js";
 
 const Wrapper = (props: { children: JSX.Element }) => {
   return (
-    <div class="flex flex-wrap justify-center gap-5 p-2">{props.children}</div>
+    <div class="flex flex-col justify-center max-[1100px]:items-center gap-5 p-2 min-[1100px]:flex-row-reverse">
+      {props.children}
+    </div>
   );
 };
 export default Wrapper;
