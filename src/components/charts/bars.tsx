6404c890f92038a1cc68835c03cbbf5c26122926
Yael <srjean21@gmail.com>
2025-02-24 23:27:17 -0400

    (adds) Se añadió este componente para mostrar gráficas de barras.

---
+++ src/components/charts/bars.tsx
@@ -0,0 +1,109 @@
+import { createEffect, createSignal, JSX } from "solid-js";
+import Chart from ".";
+import { Chart as ChartJS } from "chart.js";
+import { useTheme } from "../../hooks/useTheme";
+
+type BarProps = {
+  class?: string;
+  title: string;
+  labels: string[];
+  data: number[];
+  colors?: string | string[];
+  indexAxis?: "x" | "y";
+  children?: JSX.Element;
+};
+
+const { theme } = useTheme(),
+  media = window.matchMedia("(prefers-color-scheme: dark)");
+
+const matchTheme = (dark: boolean, chart: ChartJS) => {
+  if (!chart) return;
+  const lines = [
+      chart.options.scales!.y!.grid!,
+      chart.options.scales!.x!.grid!,
+    ],
+    text = [chart.options.scales!.y!.ticks!, chart.options.scales!.x!.ticks!];
+
+  if (dark) {
+    lines.forEach((l) => (l.color = "hsl(0,0%,25%)"));
+    text.forEach((l) => (l.color = "hsl(0,0%,70%)"));
+  } else {
+    lines.forEach((l) => (l.color = "rgb(0,0,0,0.1)"));
+    text.forEach((l) => (l.color = "hsl(0,0%,50%)"));
+  }
+
+  chart.update();
+};
+
+const Bars = (props: BarProps) => {
+  const [chart, setChart] = createSignal<ChartJS>();
+
+  createEffect(() => {
+    if (theme() === "dark") matchTheme(true, chart() as ChartJS);
+    else if (theme() === "light") matchTheme(false, chart() as ChartJS);
+    else matchTheme(media.matches, chart() as ChartJS);
+  });
+
+  if (theme() === "system") {
+    media.addEventListener("change", (e) =>
+      matchTheme(e.matches, chart() as ChartJS)
+    );
+  }
+
+  return (
+    <Chart
+      data={props.data}
+      type="bar"
+      class={props.class}
+      title={props.title}
+      // @ts-ignore
+      setChart={setChart}
+      w={250}
+      chartConfig={{
+        data: {
+          labels: props.labels,
+          datasets: [
+            {
+              label: "",
+              data: props.data,
+              backgroundColor: "rgba(70, 192, 192, 0.6)",
+              borderColor: props.colors || "rgba(150, 100, 255, 1)",
+            },
+          ],
+        },
+        options: {
+          indexAxis: props.indexAxis,
+          plugins: {
+            legend: {
+              display: false,
+            },
+            tooltip: {
+              callbacks: {
+                label: function (context) {
+                  console.log(context);
+
+                  const currentValue = context.raw as number,
+                    total = context.dataset.data.reduce(
+                      (a, b) => (a as number) + (b as number),
+                      0
+                    );
+
+                  if (!total) return " " + currentValue;
+
+                  const percentage = parseFloat(
+                    ((currentValue / (total as number)) * 100).toFixed(1)
+                  );
+
+                  return ` ${currentValue} — (${percentage}%)`;
+                },
+              },
+            },
+          },
+        },
+      }}
+    >
+      {props.children}
+    </Chart>
+  );
+};
+export default Bars;
