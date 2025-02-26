f4e5e98a545d3736d4fb87e653781b90ffc69387
Yael <srjean21@gmail.com>
2025-02-25 21:57:11 -0400

    (changes) Se actualizaron y corrigieron los tipos.
    (adds) Se añadió la prop "multiple".
    
    (changes) Ahora se evita cambiar el value cuando se indica que no puede ser null.
    
    (changes) Ahora se cambia la value en el handler, en lugar de un efecto.

--- src/components/toggle-group.tsx
+++ src/components/toggle-group.tsx
@@ -2,46 +2,58 @@ import {
   ToggleGroup as Group,
   ToggleGroupRootProps,
 } from "@kobalte/core/toggle-group";
-import { createEffect, createSignal, For, JSX } from "solid-js";
+import { createSignal, For, JSX } from "solid-js";
 
 type Option = { label: JSX.Element; value: string };
-type Value = string[] | Option[];
-type CValue<N extends true | undefined> = N extends true
-  ? null | string | string[]
-  : string | string[];
+type Options = string[] | Option[];
+
+type Value<M extends boolean, N extends true | undefined> = M extends true
+  ? string[]
+  : N extends undefined
+  ? string | null
+  : string;
 
 type ToggleGroupProps<
-  T extends Value,
-  N extends true | undefined
-> = ToggleGroupRootProps & {
+  O extends Options,
+  N extends true | undefined,
+  M extends boolean = false
+> = Omit<ToggleGroupRootProps, "value" | "onChange"> & {
   class?: string;
-  options: T;
+  options: O;
+  defaultValue?: undefined | Value<M, N>;
   notNull?: N;
-  multiple?: boolean;
-  onChange?: (value: CValue<N>) => void;
+  multiple?: M;
+  onChange?: (
+    value: M extends false ? Value<false, N> : Value<true, N>
+  ) => void;
 };
 
-const ToggleGroup = <T extends Value, N extends true | undefined>(
-  props: ToggleGroupProps<T, N>
+const ToggleGroup = <
+  O extends Options,
+  N extends true | undefined,
+  M extends boolean = false
+>(
+  props: ToggleGroupProps<O, N, M>
 ) => {
-  const [value, setValue] = createSignal<CValue<N> | undefined>(
+  const [value, setValue] = createSignal<Value<M, N> | undefined>(
     props.defaultValue
   );
-
-  createEffect(() => {
-    props.onChange && props.onChange(value()!);
-  });
 
   return (
     <Group
       {...props}
-      // @ts-expect-error strip prop
       options={null}
       class={`flex gap-2 overflow-auto min-h-max *:min-w-max ${
         props.class || ""
       }`}
       value={value()}
-      onChange={setValue}
+      // @ts-expect-error
+      onChange={(v: M extends true ? string[] : Value<false, N>) => {
+        if (v === null && props.notNull) return;
+        // @ts-expect-error
+        setValue(v as Value<M, N>);
+      }}
+      multiple={props.multiple}
     >
       <For each={props.options}>
         {(option) => (
