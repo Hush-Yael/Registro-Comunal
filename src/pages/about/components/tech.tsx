13141beeb024e817831b22de4daea45715df3f41
Yael <srjean21@gmail.com>
2025-02-26 23:18:16 -0400

    (changes) Se cambiaron el contenedor y los elementos para que sean acordes a una lista

--- src/pages/about/components/tech.tsx
+++ src/pages/about/components/tech.tsx
@@ -8,13 +8,13 @@ type Props = {
 
 const Tech = (props: Props) => {
   return (
-    <div class="grid grid-cols-[auto_1fr] items-center gap-3.5 gray-container-100">
+    <li class="grid grid-cols-[auto_1fr] items-center gap-3.5 gray-container-100">
       <props.icon class="h-[2.5em]" />
       <div>
         <span class="font-bold">{props.title}</span>
         <p class="text-sm fore">{props.description}</p>
       </div>
-    </div>
+    </li>
   );
 };
 export default Tech;
