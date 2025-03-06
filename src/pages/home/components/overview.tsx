e465d93b844a95b783d042ad2056d3af489b346f
Yael <srjean21@gmail.com>
2025-03-06 16:40:37 -0400

    (changes & adds) Se cambiaron los íconos por sus versiones filled.
    (changes) Se cambiaron los estilos de los links y sus elementos.

--- src/pages/home/components/overview.tsx
+++ src/pages/home/components/overview.tsx
@@ -1,8 +1,13 @@
 import { createResource, For, Show, Suspense } from "solid-js";
 import { plural } from "../../../lib/utils";
-import { Family } from "../../../icons/form";
-import { Id, Box, Gas, Person } from "../../../icons/index";
-import { Home } from "../../../icons/aside";
+import { FamilyFilled } from "../../../icons/form";
+import {
+  GasFilled,
+  BoxFilled,
+  IdFilled,
+  PersonFilled,
+} from "../../../icons/index";
+import { HomeFilled } from "../../../icons/aside";
 import Hr from "../../../components/layout/hr";
 import { RecordKey } from "../../../types/form";
 import { getOverview } from "../../../lib/db";
@@ -10,10 +15,10 @@ import { getOverview } from "../../../lib/db";
 const cards = [
   {
     colors:
-      "border-red-400 dark:border-red-200 text-red-950 dark:text-red-100 bg-[hsl(0,80%,98%)] hover:outline-red-400 dark:bg-[hsl(0,10%,12%)] dark:hover:outline-red-200",
+      "hover:bg-red-50/20 dark:hover:bg-red-50/10 hover:outline-red-500 dark:hover:outline-red-200 **:!fill-[hsl(0,60%,50%)]",
     label: (
       <>
-        <Person />
+        <PersonFilled />
         Jefes
       </>
     ),
@@ -21,10 +26,10 @@ const cards = [
   },
   {
     colors:
-      "border-orange-400 dark:border-orange-200 text-orange-950 dark:text-orange-100 bg-[hsl(30,80%,98%)] hover:outline-orange-400 dark:bg-[hsl(30,10%,12%)] dark:hover:outline-orange-200",
+      "hover:bg-orange-50/20 dark:hover:bg-orange-50/10 hover:outline-orange-500 dark:hover:outline-orange-200 **:!fill-[hsl(30,100%,60%)]",
     label: (
       <>
-        <Home />
+        <HomeFilled />
         Viviendas
       </>
     ),
@@ -32,10 +37,10 @@ const cards = [
   },
   {
     colors:
-      "border-yellow-400 dark:border-yellow-200 text-yellow-950 dark:text-yellow-100 bg-[hsl(60,80%,98%)] hover:outline-yellow-400 dark:bg-[hsl(60,10%,12%)] dark:hover:outline-yellow-200",
+      "hover:bg-yellow-50/20 dark:hover:bg-yellow-50/10 hover:outline-yellow-500 dark:hover:outline-yellow-200 **:!fill-[hsl(50,85%,50%)]",
     label: (
       <>
-        <Family />
+        <FamilyFilled />
         Cargas Familiares
       </>
     ),
@@ -43,10 +48,10 @@ const cards = [
   },
   {
     colors:
-      "border-emerald-400 dark:border-emerald-200 text-emerald-950 dark:text-emerald-100 bg-[hsl(110,80%,98%)] hover:outline-emerald-400 dark:bg-[hsl(110,10%,12%)] dark:hover:outline-emerald-200",
+      "hover:bg-emerald-50/20 dark:hover:bg-emerald-50/10 hover:outline-emerald-500 dark:hover:outline-emerald-200 **:!fill-emerald-500",
     label: (
       <>
-        <Id />
+        <IdFilled />
         Poseedores de Carnet
       </>
     ),
@@ -54,10 +59,10 @@ const cards = [
   },
   {
     colors:
-      "border-cyan-400 dark:border-cyan-200 text-cyan-950 dark:text-cyan-100 bg-[hsl(180,180%,98%)] hover:outline-cyan-400 dark:bg-[hsl(180,10%,12%)] dark:hover:outline-cyan-200",
+      "hover:bg-cyan-50/20 dark:hover:bg-cyan-50/10 hover:outline-cyan-500 dark:hover:outline-cyan-200 **:!fill-cyan-500",
     label: (
       <>
-        <Box />
+        <BoxFilled />
         Beneficiados del Clap
       </>
     ),
@@ -65,10 +70,10 @@ const cards = [
   },
   {
     colors:
-      "border-purple-400 dark:border-purple-200 text-purple-950 dark:text-purple-100 bg-[hsl(250,80%,98%)] hover:outline-purple-400 dark:bg-[hsl(250,10%,12%)] dark:hover:outline-purple-200",
+      "hover:bg-purple-50/20 dark:hover:bg-purple-50/10 hover:outline-purple-500 dark:hover:outline-purple-200 **:!fill-purple-500",
     label: (
       <>
-        <Gas />
+        <GasFilled />
         Beneficiados del Gas Comunal
       </>
     ),
@@ -87,15 +92,15 @@ const Overview = () => {
         {({ label, key, colors }) => (
           <a
             href={`/registros?tab=${key}`}
-            class={`flex flex-col justify-between gap-1 !p-3 rounded-xl border-1 text-center w-3/10 max-w-[200px] outline-1 hover:scale-101 outline-transparent ${
+            class={`flex flex-col justify-between gap-1 w-3/10 max-w-[200px] !p-3 rounded-xl outline-1 hover:outline-2 outline-neutral-400 dark:outline-neutral-700 hover:scale-101 dark:bg-[hsl(0,0%,13%)] ${
               colors || ""
-            } shadow dark:!shadow-[0_2px_2px_rgba(0,0,0,.5)]`}
+            } **:transition-colors text-center shadow-md dark:!shadow-[0_2px_2px_rgba(0,0,0,.5)]`}
             style={{
               transition:
-                "border-color 0.3s ease-in-out, color 0.3s ease-in-out, background-color 0.2s ease-in-out, outline-color 0.3s ease-in-out, transform 0.1s ease-in-out, scale 0.1s ease-in-out",
+                "color 0.3s ease-in-out, background-color 0.3s ease-in-out, outline-color 0.3s ease-in-out, transform 0.1s ease-in-out, scale 0.1s ease-in-out",
             }}
           >
-            <span class="flex flex-col p-1 px-2 font-bold *:min-h-[1.5em]">
+            <span class="flex flex-col gap-0.5 p-1 font-[500] px-2 *:min-h-[1.5em]">
               {label}
             </span>
             <Hr />
