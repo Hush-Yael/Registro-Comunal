b95cb90ed73f27096686faf6191604bb98443622
Yael <srjean21@gmail.com>
2025-03-12 12:53:21 -0400

    (changes) Ya no se crean dos columnas a los 1200px.
    (adds) Se limitó el tamaño de la sección hasta los 1100px.

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -2,13 +2,11 @@ import { useMedia } from "../../hooks/useMedia";
 import DB from "./components/db";
 import Info from "./components/info";
 
-export const breakpoint = useMedia(
-  "(min-width: 1024px) and (max-width: 1199px)"
-);
+export const breakpoint = useMedia("(min-width: 1024px)");
 
 const Home = () => {
   return (
-    <main class="flex flex-col flex-1 gap-8 p-3 md:p-5 min-[1200px]:grid min-[1200px]:grid-cols-2">
+    <main class="flex flex-col flex-1 gap-8 m-auto w-full p-3 md:p-5 max-w-[1100px]">
       <DB />
       <Info />
     </main>
