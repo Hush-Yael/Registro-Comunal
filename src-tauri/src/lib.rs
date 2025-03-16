624effdb89725b119d7aff7ac2b60adc23572adb
Yael <srjean21@gmail.com>
2025-03-16 13:05:05 -0400

    (changes) Se formatearon los tipos de los campos faltantes.

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -7,37 +7,37 @@ pub fn run() {
         description: "create_initial_tables",
         sql: r#"
               CREATE TABLE IF NOT EXISTS jefe (
-                cedula integer PRIMARY KEY UNIQUE NOT NULL,
-                sexo text,
-                nombres text,
-                apellidos text,
-                fechaNacimiento text,
+                cedula INTEGER PRIMARY KEY UNIQUE NOT NULL,
+                sexo TEXT,
+                nombres TEXT,
+                apellidos TEXT,
+                fechaNacimiento TEXT,
                 fechaDeceso	TEXT,
                 fallecido	INTEGER NOT NULL DEFAULT 0,
-                tel text,
-                email text,
-                edoCivil text,
-                venezolano integer DEFAULT 1,
-                nivelEstudios text,
+                tel TEXT,
+                email TEXT,
+                edoCivil TEXT,
+                venezolano INTEGER DEFAULT 1,
+                nivelEstudios TEXT,
                 FOREIGN KEY (cedula) REFERENCES jefe(cedula) ON UPDATE CASCADE ON DELETE CASCADE
               );
 
               CREATE TABLE IF NOT EXISTS gas (
-                cedula integer UNIQUE NOT NULL,
-                posee integer,
-                '10kg' integer,
-                '18kg' integer,
-                '27kg' integer,
-                '43kg' integer,
+                cedula INTEGER UNIQUE NOT NULL,
+                posee INTEGER,
+                '10kg' INTEGER,
+                '18kg' INTEGER,
+                '27kg' INTEGER,
+                '43kg' INTEGER,
                 FOREIGN KEY (cedula) REFERENCES jefe(cedula) ON UPDATE CASCADE ON DELETE CASCADE
               );
 
-              CREATE TABLE IF NOT EXISTS viviendas (
-                cedula integer NOT NULL,
-                calle text,
-                avenida text,
-                referencia text,
-                numCasa text,
+              CREATE table IF NOT EXISTS viviendas (
+                cedula INTEGER NOT NULL,
+                calle TEXT,
+                avenida TEXT,
+                referencia TEXT,
+                numCasa TEXT,
                 id TEXT NOT NULL UNIQUE,
                 PRIMARY KEY(id),
                 FOREIGN KEY (cedula) REFERENCES jefe(cedula) ON UPDATE CASCADE ON DELETE CASCADE
@@ -55,22 +55,22 @@ pub fn run() {
               );
 
               CREATE TABLE IF NOT EXISTS carnet (
-                cedula integer UNIQUE NOT NULL,
-                posee integer,
+                cedula INTEGER UNIQUE NOT NULL,
+                posee INTEGER,
                 FOREIGN KEY (cedula) REFERENCES jefe(cedula) ON UPDATE CASCADE ON DELETE CASCADE
               );
 
               CREATE TABLE IF NOT EXISTS cargaFamiliar (
                 cedula INTEGER PRIMARY KEY UNIQUE NOT NULL,
                 jefeCedula INTEGER NOT NULL,
-                sexo text,
-                nombres text,
-                apellidos text,
-                fechaNacimiento text,
+                sexo TEXT,
+                nombres TEXT,
+                apellidos TEXT,
+                fechaNacimiento TEXT,
                 fechaDeceso	TEXT,
                 fallecido	INTEGER NOT NULL DEFAULT 0,
                 venezolano INTEGER NOT NULL DEFAULT 1,
-                parentesco text,
+                parentesco TEXT,
                 FOREIGN KEY (jefeCedula) REFERENCES jefe(cedula) ON UPDATE CASCADE ON DELETE CASCADE
               );
 
